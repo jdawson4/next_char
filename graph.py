9649e6e41f1b784d4268dc5e39cbed2a5ccacf96
Jacob Dawson <jacob.dawson@me.com>
2024-04-04 22:47:27 -0400

    add graphing functionality via np logic

--- graph.py
+++ graph.py
@@ -4,10 +4,14 @@
 # our training. We'll then create a graph depicting what the expected next char
 # is! This might just be a straight display of our matrix, or it might be more
 # complex, like two columns of characters, mapping from the ones on the left to
-# the ones on the right.
+# the ones on the right. Anyway, these graphs will get saved to graphs/
 
 import argparse
 import json
+import numpy as np
+from matplotlib import pyplot as plt
+
+charsToCareAbout = "abcdefghijklmnopqrstuvwxyz"
 
 
 def findNextChar(trainingOutput, thisChar):
@@ -17,12 +21,30 @@ def findNextChar(trainingOutput, thisChar):
         if v > nextCharProb:
             nextChar = k
             nextCharProb = v
-    return nextChar
+    return nextChar, nextCharProb
 
 
 def graphFromFile(filename):
     with open("trained_dicts/" + filename) as f:
         trainingOutput = json.load(f)
+
+    # IDEA 1: display as an ndarray via matplotlib!
+    probArray = np.empty([26, 26], dtype=np.float32)
+    indexByChar = {charsToCareAbout[i]: i for i in range(len(charsToCareAbout))}
+
+    for ch1 in charsToCareAbout:
+        for ch2 in charsToCareAbout:
+            if ch1 in trainingOutput:
+                if ch2 in trainingOutput[ch1]:
+                    probArray[indexByChar[ch1]][indexByChar[ch2]] = trainingOutput[ch1][
+                        ch2
+                    ]
+    # print(probArray)
+    plt.imshow(probArray, interpolation="nearest")
+    plt.yticks(range(len(charsToCareAbout)), charsToCareAbout)
+    plt.xticks(range(len(charsToCareAbout)), charsToCareAbout)
+    # plt.show()
+    plt.savefig("graphs/" + filename[:-5] + "_matrix.png")
 
 
 if __name__ == "__main__":
@@ -40,4 +62,4 @@ if __name__ == "__main__":
     if not args.filename:
         raise Exception("Error: arg filename not provided!")
 
-    results = graphFromFile(args.filename, args.length, args.startingChar)
+    results = graphFromFile(args.filename)
